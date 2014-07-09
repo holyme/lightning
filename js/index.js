a92dd86259bdfb6219fc15eff28eb4613aaf781f
nuo.xun <nuo.xun@alibaba-inc.com>
2014-07-09 15:59:57 +0800

    backgroundColor input

--- js/index.js
+++ js/index.js
@@ -241,4 +241,43 @@
     getTpl();
 
 
+    // 获取背景颜色 todo:获取图片像素需要解决跨域问题
+    /*
+    function findPos(obj) {
+        var curleft = 0, curtop = 0;
+        if (obj.offsetParent) {
+            do {
+                curleft += obj.offsetLeft;
+                curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return { x: curleft, y: curtop };
+        }
+        return undefined;
+    }
+
+    function rgbToHex(r, g, b) {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
+    }
+
+
+    // set up some squares
+    setTimeout(function(){
+    $('#canvas').mousemove(function(e) {
+        var pos = findPos(this);
+        var x = e.pageX - pos.x;
+        var y = e.pageY - pos.y;
+        var coord = "x=" + x + ", y=" + y;
+        var c = this.getContext('2d');
+        var p = c.getImageData(x, y, 1, 1).data; 
+        var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+        $('#page-background-color').val(hex);
+    });
+    */
+
+
+},1000);
+
+
 })()
